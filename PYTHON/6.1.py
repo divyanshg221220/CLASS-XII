96985ee15ade8f46536b6f916cf63d75eb13f226
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-24 01:01:43 +0530

    .

---
+++ PYTHON/6.1.py
@@ -0,0 +1,14 @@
+# 6.1
+def prime(n,divisor=2):
+    if n<=1:
+        return False
+    if divisor*divisor>n:
+        return True
+    if n%divisor==0:
+        return False
+    return prime(n,divisor+1)
+n=int(input("Enter a number : "))
+if prime(n):
+    print(n,"is prime")
+else:
+    print(n,"is not prime")
