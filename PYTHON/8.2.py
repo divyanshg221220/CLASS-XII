f9b3cf405ce4709d1c5ef44c7a3040c70b33158c
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-24 21:29:08 +0530

    .

---
+++ PYTHON/8.2.py
@@ -0,0 +1,12 @@
+#8.2
+def unique(lst):
+    if lst!=[]:
+        list=[]
+        for i in lst:
+            if i not in list:
+                list.append(i)
+        print(list)
+    else:
+        print("Empty list")
+lst=eval(input("Enter a list : "))
+unique(lst)
