930751284957021bebd16b96a9c90648f7a3c82e
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-25 21:18:07 +0530

    .

--- PYTHON/9.1.py
+++ PYTHON/9.1.py
@@ -2,10 +2,13 @@
 stack=eval(input("Enter a list: "))
 q=int(input("Choose an operation to perform:\n1. push\n2. pop\nEnter your choice (1-2): "))
 if q==1:
-    n=int(input("Enter a number"))
+    n=int(input("Enter a number: "))
     stack.append(n)
 elif q==2:
-    stack.pop()
+    if stack!=[]:
+        stack.pop()
+    else:
+        print("Underflow")
 else:
     print("Invalid input")
 print(stack)
