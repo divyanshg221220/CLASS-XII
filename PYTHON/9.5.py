f283a131c541c854a10f8d1951beb6f4f96c64a1
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-25 21:27:40 +0530

    .

---
+++ PYTHON/9.5.py
@@ -0,0 +1,14 @@
+#9.5
+queue=eval(input("Enter a list: "))
+q=int(input("Choose an operation to perform:\n1. push\n2. pop\nEnter your choice (1-2): "))
+if q==1:
+    n=int(input("Enter a number: "))
+    queue.append(n)
+elif q==2:
+    if queue!=[]:
+        queue.pop(0)
+    else:
+        print("Underflow")
+else:
+    print("Invalid input")
+print(queue)
