e4d361bf530a83db7ce773b4a9e0ba82ec772e00
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-27 02:13:20 +0530

    .

---
+++ PYTHON/9.13.py
@@ -0,0 +1,12 @@
+#9.13
+Arr=eval(input("Enter a list: "))
+stack=[]
+def PUSH(Arr):
+    for i in Arr:
+        if i%5==0:
+            stack.append(i)
+PUSH(Arr)
+if stack!=[]:
+    print(stack)
+else:
+    print("Empty stack")
