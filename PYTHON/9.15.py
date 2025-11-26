be3dd727c56f73bf29b6a3cf7d3670d0ee3ccdc3
divyanshg221220 <divyanshg221220@outlook.com>
2025-11-27 02:37:39 +0530

    .

--- PYTHON/9.15.py
+++ PYTHON/9.15.py
@@ -1,24 +1,24 @@
-#9.10
+#9.15
 Stack=[]
-def PUSH(Books):
-    Stack.append(Books)
-def POP(Books):
-    Stack.pop(Stack.index(Books))
+def Addnew(Book):
+    Stack.append(Book)
+def Remove(Book):
+    Stack.pop(Stack.index(Book))
 while True:
     print()
-    print("1. PUSH(Books)")
-    print("2. POP(Books)")
+    print("1. Addnew(Book)")
+    print("2. Remove(Book)")
     print("3. EXIT")
     print()
     choice=int(input("USER'S CHOICE: "))
     if choice==1:
-        Books=input("Enter a book: ")
-        PUSH(Books)
+        Book=input("Enter a book: ")
+        Addnew(Book)
     elif choice==2:
         if Stack!=[]:
-            Books=input("Enter a book: ")
-            if Books in Stack:
-                POP(Books)
+            Book=input("Enter a book: ")
+            if Book in Stack:
+                Remove(Book)
             else:
                 print("Book not found")
         else:
